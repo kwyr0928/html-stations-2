aedbd5ac40cf7a9af0c6b17bda1282e7547c7601
kwyr0928 <yuri.kawase.0928@gmail.com>
2024-04-15 02:10:06 +0900

    station13 clear

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,11 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const text = document.getElementById("text");
+if(check.checked){
+  text.style.backgroundColor = 'red';
+}else{
+  text.style.backgroundColor = 'white';
 }
+}
+
+// document.getElementById("text");
